07f3856fa51c87a184f2badc133c38ed9c521806
sshams <muizz@saads-mbp.gateway.pace.com>
2014-09-27 23:36:30 -0400

    init

--- js/ApplicationFacade.js
+++ js/ApplicationFacade.js
@@ -1,6 +1,6 @@
 /*
  PureMVC MultiCore Demo for JS - Sequential
- Copyright(c) 2014 Saad Shams <saad.shams@puremvc.org>
+ Copyright(c) 2014 Saad Shams, Cliff Hall
  Your reuse is governed by the Creative Commons Attribution 3.0 License
 */
 puremvc.define(
--- js/controller/ExampleSimpleCommand.js
+++ js/controller/ExampleSimpleCommand.js
@@ -1,6 +1,6 @@
 /*
  PureMVC MultiCore Demo for JS - Sequential
- Copyright(c) 2014 Saad Shams <saad.shams@puremvc.org>
+ Copyright(c) 2014 Saad Shams
  Your reuse is governed by the Creative Commons Attribution 3.0 License
 */
 puremvc.define(
